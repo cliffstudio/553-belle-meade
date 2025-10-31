806823703ed567f84f200f4ce07d68e223069dee
Jodie <jodie@cliff.studio>
2025-10-31 09:16:59 +0000

    Update VirtualTourEmbed component and styles for improved responsiveness and functionality
    - Changed iframe source to the latest version.
    - Adjusted iframe height to 100% for better adaptability.
    - Added styles for the virtual tour component to maintain aspect ratio and responsiveness on different screen sizes.

--- src/components/VirtualTourEmbed.tsx
+++ src/components/VirtualTourEmbed.tsx
@@ -9,17 +9,10 @@ export default function VirtualTourEmbed({ className }: VirtualTourEmbedProps) {
     <div id="virtual-tour" 
       className={`${className} out-of-view`}
     >
-      <iframe src="//storage.net-fs.com/hosting/7298008/14/" 
+      <iframe src="//storage.net-fs.com/hosting/7298008/15/" 
         width="100%"
-        height="700"
-        frameBorder="0"
-        allow="fullscreen; accelerometer; gyroscope; vr; camera; microphone" allowFullScreen={true}
-        style={{
-          width: '100%',
-          height: '600px',
-          border: 'none',
-          overflow: 'hidden',
-        }}
+        height="100%"
+        allow="fullscreen; accelerometer; gyroscope; magnetometer; vr; xr; xr-spatial-tracking; autoplay; camera; microphone" allowFullScreen={true}
       />
     </div>
   )
