ac0745d7c0cb6c4df8f06c65a4a8e1d10372076b
Jodie <jodie@cliff.studio>
2025-10-31 11:21:58 +0000

    Add dummy list form to Footer component and update styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,6 +14,21 @@ export default function Footer({ footer }: FooterProps) {
   return (
     <footer className="site-footer h-pad">
       <div className="top-row row-lg">
+        {/* todo: hook up to Mailchimp */}
+        <div className="column-3 col-3-12_lg mobile">
+          <div className="mailing-list-form">
+            <div className="input-wrap">
+              <input type="text" placeholder="Email Address" />
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12">
+                <path d="M0.353516 0.353516L5.85352 5.85352L0.353516 11.3535"/>
+              </svg>
+            </div>
+
+            <p>By signing up, you agree to our <Link href="/privacy-policy">privacy policy</Link></p>
+          </div>
+        </div>
+        
         <div className="column-1 col-3-12_lg">
           {footer.footerItems && footer.footerItems.map((item, index) => (
             <div key={index}>
@@ -100,8 +115,20 @@ export default function Footer({ footer }: FooterProps) {
           </div>
         )}
 
-        {/* todo: mailing list form */}
-        {/* <div className="col-3-12_lg"></div> */}
+        {/* todo: hook up to Mailchimp */}
+        <div className="column-3 col-3-12_lg desktop">
+          <div className="mailing-list-form">
+            <div className="input-wrap">
+              <input type="text" placeholder="Email Address" />
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12">
+                <path d="M0.353516 0.353516L5.85352 5.85352L0.353516 11.3535"/>
+              </svg>
+            </div>
+
+            <p>By signing up, you agree to our <Link href="/privacy-policy">privacy policy</Link></p>
+          </div>
+        </div>
       </div>
 
       <div className="logo desktop">
