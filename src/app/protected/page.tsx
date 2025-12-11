c48fc9e82368bec099faca6fc49b0f9752438096
Jodie <jodie@cliff.studio>
2025-12-11 12:52:24 +0000

    Add authentication middleware and session management
    - Introduced `iron-session` for session handling, enhancing security and user authentication.
    - Created middleware to protect routes and redirect unauthenticated users to the sign-in page.
    - Developed sign-in page with password authentication and session management.
    - Added utility functions for session retrieval and authentication state management.
    - Updated styles for the sign-in component to improve user experience.

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,17 @@
+import { getSession } from "@/sanity/utils/auth";
+import { redirect } from "next/navigation";
+
+export default async function Protected() {
+  const session = await getSession();
+
+  if (!session.isAuthenticated) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <main>
+      <h1>🔒 Protected page</h1>
+      <p>This page is password protected!</p>
+    </main>
+  );
+}
