ebf5f9626f1ba30bb9c55c635cecf3d2eb212f77
Jodie <jodie@cliff.studio>
2025-12-15 10:00:56 +0000

    Refactor TextBlock component layout for improved structure

--- src/components/TextBlock.tsx
+++ src/components/TextBlock.tsx
@@ -10,14 +10,10 @@ type TextBlockProps = {
 export default function TextBlock({ heading, body }: TextBlockProps) {
   return (
     <section className="text-block h-pad">
-      <div className="row-lg">
-        <div className="col-6-12_lg out-of-view">
-          <div className="text-wrap">
-            {heading && <div className="heading cta-font">{heading}</div>}
-            
-            {body && <h2><PortableText value={body} components={portableTextComponents} /></h2>}
-          </div>
-        </div>
+      <div className="text-wrap out-of-view">
+        {heading && <div className="heading cta-font">{heading}</div>}
+        
+        {body && <h2><PortableText value={body} components={portableTextComponents} /></h2>}
       </div>
     </section>
   )
